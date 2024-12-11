54d24f20e114b53380b7de80f4ac2292552cb841
matus <1matus.gabas@gmail.com>
2024-12-12 00:12:31 +0100

    navigation, dark mode switch + added root color and font variables

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 "use strict";
 
-const light_mode = document.getElementById("light-mode");
-const dark_mode = document.getElementById("dark-mode");
+const mode_switch = document.getElementById("switch");
+const root = document.documentElement;
 
-light_mode.addEventListener("click", () => {
-  // console.log("aha");
-  document.body.style = "background: linear-gradient(circle, rgba(255, 255, 255, 1) 0%, rgba(232, 232, 232, 1) 50%, rgba(255, 255, 255, 1) 100%);"
-});
-
-dark_mode.addEventListener("click", () => {
-  document.body.style = "background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(20,20,20,1) 50%, rgba(0,0,0,1) 100%) fixed;"
+let is_light_mode = true;
+mode_switch.addEventListener("click", () => {
+  if (is_light_mode) {
+    root.style.setProperty('--main-color', '#181818');
+    root.style.setProperty('--text', '#ffffff');
+    root.style.setProperty('--text-alt', '#0b0b0b');
+    root.style.setProperty('--switch', '#ffffff');
+    mode_switch.textContent = "svetlý";
+  }
+  else {
+    root.style.setProperty('--main-color', '#ebebeb');
+    root.style.setProperty('--text', '#0b0b0b');
+    root.style.setProperty('--text-alt', '#ffffff');
+    root.style.setProperty('--switch', '#0b0b0b');
+    mode_switch.textContent = "tmavý";
+  }
+  is_light_mode = !is_light_mode;
 });
