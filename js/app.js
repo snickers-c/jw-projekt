7ee06fee351bceab807f1311ae3948546880a386
matus <1matus.gabas@gmail.com>
2024-12-12 19:33:04 +0100

    navigation prototype finished + added future pages files

--- js/app.js
+++ js/app.js
@@ -10,14 +10,21 @@ mode_switch.addEventListener("click", () => {
     root.style.setProperty('--text', '#ffffff');
     root.style.setProperty('--text-alt', '#0b0b0b');
     root.style.setProperty('--switch', '#ffffff');
-    mode_switch.textContent = "svetlý";
+    mode_switch.textContent = "Svetlý";
   }
   else {
     root.style.setProperty('--main-color', '#ebebeb');
     root.style.setProperty('--text', '#0b0b0b');
     root.style.setProperty('--text-alt', '#ffffff');
     root.style.setProperty('--switch', '#0b0b0b');
-    mode_switch.textContent = "tmavý";
+    mode_switch.textContent = "Tmavý";
   }
   is_light_mode = !is_light_mode;
 });
+
+const hamburger = document.querySelector(".hamburger");
+const nav_links = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  nav_links.classList.toggle("show");
+})
