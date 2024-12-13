e7c859c1af656b83c32d2e15f93ddf98a0f832e8
matus <1matus.gabas@gmail.com>
2024-12-14 00:58:17 +0100

    fixed nav dropdown on mobile + improved dark/light mode switch

--- js/app.js
+++ js/app.js
@@ -3,7 +3,37 @@
 const mode_switch = document.getElementById("switch");
 const root = document.documentElement;
 
-let is_light_mode = true;
+let is_light_mode;
+if (localStorage.getItem("is_light_mode") === null) {
+  localStorage.setItem("is_light_mode", true);
+  is_light_mode = true;
+} else {
+  is_light_mode = JSON.parse(localStorage.getItem("is_light_mode"));
+  if (!is_light_mode) {
+    // dark mode
+    root.style.setProperty('--main-color', '#181818');
+    root.style.setProperty('--text', '#ffffff');
+    root.style.setProperty('--text-alt', '#0b0b0b');
+    root.style.setProperty('--switch', '#ffffff');
+    root.style.setProperty('--nav-footer-color', '#2f2f2f');
+    root.style.setProperty('--card-color', '#000000');
+    root.style.setProperty('--card-border', '#777777');
+    root.style.setProperty('--text2', '#bbbbbb');
+    mode_switch.textContent = "Svetlý";
+  } else {
+    // light mode
+    root.style.setProperty('--main-color', '#ebebeb');
+    root.style.setProperty('--text', '#0b0b0b');
+    root.style.setProperty('--text-alt', '#ffffff');
+    root.style.setProperty('--switch', '#0b0b0b');
+    root.style.setProperty('--nav-footer-color', '#e0e0e0');
+    root.style.setProperty('--card-color', '#ffffff');
+    root.style.setProperty('--card-border', '#cccccc');
+    root.style.setProperty('--text2', '#555555');
+    mode_switch.textContent = "Tmavý";
+  }
+}
+
 mode_switch.addEventListener("click", () => {
   if (is_light_mode) { // dark mode
     root.style.setProperty('--main-color', '#181818');
@@ -15,8 +45,9 @@ mode_switch.addEventListener("click", () => {
     root.style.setProperty('--card-border', '#777777');
     root.style.setProperty('--text2', '#bbbbbb');
     mode_switch.textContent = "Svetlý";
-  }
-  else { // light mode
+    is_light_mode = false;
+    localStorage.setItem("is_light_mode", false);
+  } else { // light mode
     root.style.setProperty('--main-color', '#ebebeb');
     root.style.setProperty('--text', '#0b0b0b');
     root.style.setProperty('--text-alt', '#ffffff');
@@ -26,13 +57,22 @@ mode_switch.addEventListener("click", () => {
     root.style.setProperty('--card-border', '#cccccc');
     root.style.setProperty('--text2', '#555555');
     mode_switch.textContent = "Tmavý";
+    is_light_mode = true;
+    localStorage.setItem("is_light_mode", true);
   }
-  is_light_mode = !is_light_mode;
 });
 
 const hamburger = document.querySelector(".hamburger");
 const nav_links = document.querySelector(".nav-links");
 
-hamburger.addEventListener("click", () => {
-  nav_links.classList.toggle("show");
-})
+hamburger.addEventListener('click', () => {
+  nav_links.classList.toggle('active');
+});
+
+document.querySelectorAll('.course-card-header').forEach(header => {
+  header.addEventListener('click', () => {
+    const card = header.parentElement;
+
+    card.classList.toggle('active');
+  });
+});
