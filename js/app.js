b79df9b89642defb65474dcc91e26719020ed6d9
matus <1matus.gabas@gmail.com>
2024-12-13 22:02:06 +0100

    index page prototype finished

--- js/app.js
+++ js/app.js
@@ -11,6 +11,9 @@ mode_switch.addEventListener("click", () => {
     root.style.setProperty('--text-alt', '#0b0b0b');
     root.style.setProperty('--switch', '#ffffff');
     root.style.setProperty('--nav-footer-color', '#2f2f2f');
+    root.style.setProperty('--card-color', '#000000');
+    root.style.setProperty('--card-border', '#777777');
+    root.style.setProperty('--text2', '#bbbbbb');
     mode_switch.textContent = "Svetlý";
   }
   else { // light mode
@@ -19,6 +22,9 @@ mode_switch.addEventListener("click", () => {
     root.style.setProperty('--text-alt', '#ffffff');
     root.style.setProperty('--switch', '#0b0b0b');
     root.style.setProperty('--nav-footer-color', '#e0e0e0');
+    root.style.setProperty('--card-color', '#ffffff');
+    root.style.setProperty('--card-border', '#cccccc');
+    root.style.setProperty('--text2', '#555555');
     mode_switch.textContent = "Tmavý";
   }
   is_light_mode = !is_light_mode;
